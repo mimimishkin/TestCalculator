de1fb14e14faa02e13de9b07627366ded920b5da
mika <printf.mika@gmail.com>
2025-06-19 20:20:06 +0300

    Init tests

---
+++ TestCalculator.Tests/CalculatorControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCalculator.WebApi;
+using TestCalculator.Domain;
+
+namespace TestCalculator.Tests;
+
+public class CalculatorControllerTests
+{
+    private readonly CalculatorController _controller = new(new Calculator());
+
+    [Fact]
+    public void Add_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Add(2, 3) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(5d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Subtract_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Subtract(5, 2) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(3d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Multiply_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Multiply(4, 2) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(8d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Divide_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Divide(10, 2) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(5d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ReturnsBadRequest()
+    {
+        var result = _controller.Divide(10, 0) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(400, result.StatusCode);
+        Assert.Contains("divide by zero", result.Value!.ToString());
+    }
+    
+    [Fact]
+    public void Power_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Power(2, 3) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(8d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Root_ReturnsOkResult_WithExpectedValue()
+    {
+        var result = _controller.Root(16, 2) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(4d, (double)result.Value!.GetType().GetProperty("result")!.GetValue(result.Value)!);
+    }
+
+    [Fact]
+    public void Root_WithNegativeNumber_ReturnsBadRequest()
+    {
+        var result = _controller.Root(-16, 2) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(400, result.StatusCode);
+        Assert.Contains("Number cannot be lower than zero for even root", result.Value!.ToString());
+    }
+}
+
