1a935d0c48408cac62d9dd11de1ab1ce2a963c73
mika <printf.mika@gmail.com>
2025-06-19 20:11:38 +0300

    Add calculator controller

---
+++ TestCalculator/WebApi/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCalculator.Domain;
+
+namespace TestCalculator.WebApi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CalculatorController(ICalculator calculator) : ControllerBase
+{
+    private IActionResult HandleCalculation(Func<double> calculation)
+    {
+        try
+        {
+            var result = calculation();
+            return Ok(new { result });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+    }
+
+    [HttpGet("add")]
+    public IActionResult Add([FromQuery] double a, [FromQuery] double b)
+    {
+        return HandleCalculation(() => calculator.Add(a, b));
+    }
+
+    [HttpGet("subtract")]
+    public IActionResult Subtract([FromQuery] double a, [FromQuery] double b)
+    {
+        return HandleCalculation(() => calculator.Subtract(a, b));
+    }
+
+    [HttpGet("multiply")]
+    public IActionResult Multiply([FromQuery] double a, [FromQuery] double b)
+    {
+        return HandleCalculation(() => calculator.Multiply(a, b));
+    }
+
+    [HttpGet("divide")]
+    public IActionResult Divide([FromQuery] double a, [FromQuery] double b)
+    {
+        return HandleCalculation(() => calculator.Divide(a, b));
+    }
+}
