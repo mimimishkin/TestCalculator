1a0b43679554ccba2a73e9c857646bcaaad09883
mika <printf.mika@gmail.com>
2025-06-19 19:55:33 +0300

    Add calculator interface and impl

---
+++ TestCalculator/Domain/Calculator.cs
@@ -0,0 +1,38 @@
+﻿namespace TestCalculator.Domain;
+
+public class Calculator : ICalculator
+{
+    public double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Divide(double x, double y)
+    {
+        if (y == 0)
+            throw new DivideByZeroException();
+        return x / y;
+    }
+
+    public double Power(double baseNumber, double exponent)
+    {
+        return Math.Pow(baseNumber, exponent);
+    }
+
+    public double Root(double number, double nthRoot)
+    {
+        if (nthRoot == 0)
+            throw new ArgumentException("Root cannot be zero", nameof(nthRoot));
+        return Math.Pow(number, 1.0 / nthRoot);
+    }
+}
---
+++ TestCalculator/Domain/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace TestCalculator.Domain;
+
+public interface ICalculator
+{
+    double Add(double x, double y);
+
+    double Subtract(double x, double y);
+
+    double Multiply(double x, double y);
+
+    double Divide(double x, double y);
+
+    double Power(double baseNumber, double exponent);
+
+    double Root(double number, double nthRoot);
+}
